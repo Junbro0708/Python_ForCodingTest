36c30601ef36a6cebcc17ecda70251236a09a806
Junbro0708 <aktm708@naver.com>
2022-10-16 23:26:04 -0700

    init

---
+++ ForCodingTest/000_Init.cpp
@@ -0,0 +1,15 @@
+//
+//  000_Init.cpp
+//  ForCodingTest
+//
+//  Created by MAS on 2022-10-16.
+//
+
+//#include <iostream>
+//
+//using namespace::std;
+//
+//int main(int argc, const char * argv[]) {
+//
+//    return 0;
+//}
---
+++ ForCodingTest/002_LongestCommonPrefix.cpp
@@ -0,0 +1,25 @@
+//
+//  002_LongestCommonPrefix.cpp
+//  ForCodingTest
+//
+//  Created by MAS on 2022-10-16.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace::std;
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        return "D";
+    }
+};
+
+int main(int argc, const char * argv[]) {
+    Solution solve;
+    cout << solve.longestCommonPrefix() << "\n";
+    return 0;
+}
+
