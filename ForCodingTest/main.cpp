683366de132f7c16949ea36f3ad539d1bda5b8b0
Junbro0708 <aktm708@naver.com>
2022-10-16 22:12:40 -0700

    Initial Commit

---
+++ ForCodingTest/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ForCodingTest
+//
+//  Created by MAS on 2022-10-16.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
