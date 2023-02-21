a5d788519b5c03136afeeefadc88fc4252eaf244
Junbro0708 <aktm708@naver.com>
2023-02-20 23:12:54 -0800

    bfs

--- ForCodingTest/002_LongestCommonPrefix.cpp
+++ ForCodingTest/002_LongestCommonPrefix.cpp
@@ -5,21 +5,21 @@
 //  Created by MAS on 2022-10-16.
 //
 
-#include <iostream>
-#include <vector>
-
-using namespace::std;
-
-class Solution {
-public:
-    string longestCommonPrefix(vector<string>& strs) {
-        return "D";
-    }
-};
-
-int main(int argc, const char * argv[]) {
-    Solution solve;
-    cout << solve.longestCommonPrefix() << "\n";
-    return 0;
-}
-
+//#include <iostream>
+//#include <vector>
+//
+//using namespace::std;
+//
+//class Solution {
+//public:
+//    string longestCommonPrefix(vector<string>& strs) {
+//        return "D";
+//    }
+//};
+//
+//int main(int argc, const char * argv[]) {
+//    Solution solve;
+//    cout << solve.longestCommonPrefix() << "\n";
+//    return 0;
+//}
+//
