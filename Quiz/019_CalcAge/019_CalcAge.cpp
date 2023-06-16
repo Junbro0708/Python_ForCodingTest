88347e52c0ab4590757ac233676d95b919d72b48
Junbro0708 <aktm708@naver.com>
2023-06-16 17:34:16 +0900

    나이계산

--- Quiz/019_CalcAge/019_CalcAge.cpp
+++ Quiz/019_CalcAge/019_CalcAge.cpp
@@ -1,12 +1,19 @@
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
 int main(){
-  string str;
-  vector<string> list;
-
+  string str, age = "", gender;
   cin >> str;
-  list = split
+
+  for(int i = 0; i < str.length(); i++){
+    if(i == 0 || i == 1) age += str[i];
+    if(str[i] == '-') gender = str[i+1];
+  }
+  
+  if(str[7] == '3' || str[7] == '4') cout << 20 - stoi(age) << " ";
+  else cout << 100 - stoi(age) + 20 << " ";
+
+  if(gender == "1" || gender == "3") cout << 'M';
+  else cout << "W";
 }
