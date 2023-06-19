1a50411765451e7172e1a4b15e4e4525dcbc6b8f
Junbro0708 <aktm708@naver.com>
2023-06-19 17:29:47 +0900

    21 영어단어 복구하기

--- Quiz/021_FixWord/021_FixWord.cpp
+++ Quiz/021_FixWord/021_FixWord.cpp
@@ -1,21 +1,40 @@
-#include <iostream>
+// #include <iostream>
 
-using namespace std;
+// using namespace std;
+
+// int main(){
+//   string str, answer = "";
+
+//   getline(cin, str);
+
+//   for(int i = 0; i < str.length(); ++i){
+//     char temp;
+//     if(str[i] >= 65 && str[i] <= 90){
+//       temp = str[i] + 32;
+//       answer += temp;
+//     }else if(str[i] >= 97 && str[i] <= 122){
+//       answer += str[i];
+//     }
+//   }
+
+//   cout << answer;
+// }
+
+#include <stdio.h>
 
 int main(){
-  string str, answer = "";
+  char a[101], b[101];
+  int p = 0;
 
-  getline(cin, str);
-
-  for(int i = 0; i < str.length(); ++i){
-    char temp;
-    if(str[i] >= 65 && str[i] <= 90){
-      temp = str[i] + 32;
-      answer += temp;
-    }else if(str[i] >= 97 && str[i] <= 122){
-      answer += str[i];
+  gets(a);
+  for(int i = 0; a[i] != '\0'; ++i){
+    if(a[i] != ' '){
+      if(a[i] >= 65 && a[i] <= 90){
+        b[p++] = a[i] + 32;
+      }else b[p++] = a[i];
     }
   }
-
-  cout << answer;
+  b[p] = '\0';
+  printf("%s", b);
+  return 0;
 }
