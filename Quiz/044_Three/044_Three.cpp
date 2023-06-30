4a6e244cd68ca769ee9da56edec132b1ece5426d
Junbro0708 <aktm708@naver.com>
2023-06-30 18:27:17 +0900

    algorithm

--- Quiz/044_Three/044_Three.cpp
+++ Quiz/044_Three/044_Three.cpp
@@ -5,6 +5,15 @@
 using namespace std;
 
 int main(){
-  
+  int N, cnt = 0;
+  cin >> N;
+
+  for(int i = 3; i <= N; ++i){
+    string str = to_string(i);
+    for(int j = 0; j < str.length(); ++j){
+      if(str[j] == '3') cnt++;
+    }
+  }
+  cout << cnt << "\n";
   return 0;
 }
