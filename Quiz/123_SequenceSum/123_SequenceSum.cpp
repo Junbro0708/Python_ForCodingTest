2781ef0d759e0c6c6e99a5f1167f1f901a791975
Junbro0708 <aktm708@naver.com>
2023-08-22 13:45:02 +0900

    algorithm

---
+++ Quiz/123_SequenceSum/123_SequenceSum.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  int N, answer = -2147000000;
+  cin >> N;
+  vector<int> list, td(N, -2147000000);
+
+  for(int i = 0; i < N; ++i){
+    int temp;
+    cin >> temp;
+    list.push_back(temp);
+  }
+
+  td[0] = list[0];
+  for(int i = 1; i < list.size(); ++i){
+    td[i] = max(list[i], list[i] + td[i-1]);
+  }
+
+  for(int i = 0; i < td.size(); ++i)
+    answer = max(answer, td[i]);
+  
+  cout << answer;
+}
